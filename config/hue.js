75ba75139bc57f16866a375b33f4c4688a46f56c
Børge Antonsen <bovan79@gmail.com>
2016-06-17 21:42:45 +0200

    added basic config example

---
+++ config/hue.js
@@ -0,0 +1,8 @@
+// IP address to hue hub
+const ip = '192.168.0.100';
+
+// hue username, retrieved by registering with hue-util
+const username  = '1234567890abcdef1234567890'
+
+// light ids to use when updating without specifying light source
+const lights = [1,2,3]
