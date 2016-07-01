be741ce9db9691db13ef435b952deab1ad2d9d5e
Børge Antonsen <bovan79@gmail.com>
2016-07-01 18:28:54 +0200

    added configs

---
+++ config/hue.ts
@@ -0,0 +1,12 @@
+export const hueConfig = {
+    // IP address to hue hub
+    ip: '192.168.2.136',
+
+    // hue username, retrieved by registering with hue-util
+    username: '',
+
+    // light ids to use when updating without specifying light source
+    lights: [1,2,3],
+
+    mainLight: 3
+}
